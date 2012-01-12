61126c968b4057e83203c5719aba81605760fc25
Raphfrk <raphfrk@gmail.com>
2012-01-12 03:03:54 +0000

    Added license headers
    Signed-off-by: Raphfrk <raphfrk@gmail.com>

--- src/com/raphfrk/bukkit/eventlinkapi/EventLinkAPI.java
+++ src/com/raphfrk/bukkit/eventlinkapi/EventLinkAPI.java
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ * Copyright (C) 2012 Raphfrk
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
 package com.raphfrk.bukkit.eventlinkapi;
 
 import java.util.Set;
@@ -6,6 +27,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 
 public interface EventLinkAPI {
+	
+	/**
+	 * Returns the name of the service provider
+	 *
+	 * @return the name of the provider
+	 */
+
+	public boolean getProviderName();
 	
 	/**
 	 * Tests if a player is listed on the admin_list for the eventlink.txt file
